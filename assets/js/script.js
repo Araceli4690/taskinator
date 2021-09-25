d9486133656b9a181e5f55317c396d1cc3b7f4e9
araceli <carmona.araceli.4690@gmail.com>
2021-09-25 13:23:31 -0700

    function clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,33 @@
 let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do")
 
-let createTaskHandler = function (event) {
+let taskFormHandler = function (event) {
     event.preventDefault();
 
-    let listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+    //package data as object
+    let taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    //send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
 };
 
+let createTaskEl = function (taskDataObj) {
+    let listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    //create div to hold task infor and add to list item
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    //innerHTML works similar to textContent except we can use actual HTML elemnts
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+}
+
 //waiting for click on button el with a type attribute that has value submit
-formEl.addEventListener('submit', createTaskHandler)
+formEl.addEventListener('submit', taskFormHandler)
