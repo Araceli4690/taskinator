71aea1420a21429fe9c8322bebb606de36629735
araceli <carmona.araceli.4690@gmail.com>
2021-09-25 12:55:20 -0700

    added form handler event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-let buttonEl = document.querySelector("#save-task");
+let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do")
 
-let createTaskHandler = function () {
+let createTaskHandler = function (event) {
+    event.preventDefault();
+
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener('click', createTaskHandler)
+//waiting for click on button el with a type attribute that has value submit
+formEl.addEventListener('submit', createTaskHandler)
